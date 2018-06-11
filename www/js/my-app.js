a6534288327ac5fd276f6586cb6034b0647b3770
imbxl <imbxl@hotmail.com>
2018-06-10 22:33:58 -0300

    see

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -418,3 +418,93 @@ function HistorialVerMas(id){
 	$$('.popup-historial .descripcion_larga').show();
 	myApp.popup('.popup-historial');
 }
+
+
+
+function LocationConfigure(){
+	BackgroundGeolocation.configure({
+    locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+    desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
+    stationaryRadius: 50,
+    distanceFilter: 50,
+    notificationTitle: 'Background tracking',
+    notificationText: 'enabled',
+    debug: true,
+    interval: 10000,
+    fastestInterval: 5000,
+    activitiesInterval: 10000,
+    url: 'http://192.168.81.15:3000/location',
+    httpHeaders: {
+      'X-FOO': 'bar'
+    },
+    // customize post properties
+    postTemplate: {
+      lat: '@latitude',
+      lon: '@longitude',
+      foo: 'bar' // you can also add your own properties
+    }
+  });
+ 
+  BackgroundGeolocation.on('location', function(location) {
+    // handle your locations here
+    // to perform long running operation on iOS
+    // you need to create background task
+    BackgroundGeolocation.startTask(function(taskKey) {
+      // execute long running task
+      // eg. ajax post location
+      // IMPORTANT: task has to be ended by endTask
+      BackgroundGeolocation.endTask(taskKey);
+    });
+  });
+ 
+  BackgroundGeolocation.on('stationary', function(stationaryLocation) {
+    // handle stationary locations here
+  });
+ 
+  BackgroundGeolocation.on('error', function(error) {
+    console.log('[ERROR] BackgroundGeolocation error:', error.code, error.message);
+  });
+ 
+  BackgroundGeolocation.on('start', function() {
+    console.log('[INFO] BackgroundGeolocation service has been started');
+  });
+ 
+  BackgroundGeolocation.on('stop', function() {
+    console.log('[INFO] BackgroundGeolocation service has been stopped');
+  });
+ 
+  BackgroundGeolocation.on('authorization', function(status) {
+    console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
+    if (status !== BackgroundGeolocation.AUTHORIZED) {
+      // we need to set delay or otherwise alert may not be shown
+      setTimeout(function() {
+        var showSettings = confirm('App requires location tracking permission. Would you like to open app settings?');
+        if (showSetting) {
+          return BackgroundGeolocation.showAppSettings();
+        }
+      }, 1000);
+    }
+  });
+ 
+  BackgroundGeolocation.on('background', function() {
+    console.log('[INFO] App is in background');
+    // you can also reconfigure service (changes will be applied immediately)
+    BackgroundGeolocation.configure({ debug: true });
+  });
+ 
+  BackgroundGeolocation.on('foreground', function() {
+    console.log('[INFO] App is in foreground');
+    BackgroundGeolocation.configure({ debug: false });
+  });
+ 
+  BackgroundGeolocation.checkStatus(function(status) {
+    console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
+    console.log('[INFO] BackgroundGeolocation services enabled', status.locationServicesEnabled);
+    console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
+ 
+    // you don't need to check status before start (this is just the example)
+    if (!status.isRunning) {
+      BackgroundGeolocation.start(); //triggers start on start event
+    }
+  });
+}
